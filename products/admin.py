9686af87365810d4f8ba18ca9aaf6907e1d7889c
Vlad Oprea <opreavladteofil@gmail.com>
2020-07-03 14:10:34 +0100

    Customised admin for collections and products

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import Product, Collection
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'height',
+        'width',
+        'image'
+    )
 
-admin.site.register(Product)
-admin.site.register(Collection)
+    ordering = ('sku',)
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Collection, CollectionAdmin)
 
 
