4e8b40f4a7eb7c58b62997ca365a985c7baaefa0
Vlad Oprea <opreavladteofil@gmail.com>
2020-07-03 13:33:28 +0100

    Add products app, models and fixtures

---
+++ products/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product, Collection
+
+
+admin.site.register(Product)
+admin.site.register(Collection)
+
+
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Collection(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+
+class Product(models.Model):
+    collection = models.ForeignKey('Collection', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    height =  models.DecimalField(max_digits=6, decimal_places=2)
+    width =  models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
