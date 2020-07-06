39b658aa3d1f8b643dd110e86171a8b49c101e13
Vlad Oprea <opreavladteofil@gmail.com>
2020-07-06 14:08:15 +0100

    Add pagination view and template for all products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries.
+        Pagination included for more than 12 products on page
+    """
     
-    products = Product.objects.all()
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 12)
 
+    page_number = request.GET.get('page')
+    products = paginator.get_page(page_number)
     context = {
         'products': products,
     }
