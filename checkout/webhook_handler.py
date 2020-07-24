af701e5854e0aa8bc68e2f4c4833509f6c6c89b1
Vlad Oprea <opreavladteofil@gmail.com>
2020-07-24 19:16:56 +0100

    Add confirmation emails on completed orders

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,9 @@
 Code written following Code Institute video lessons on Stripe payments
 """
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -15,6 +18,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request 
+
+    # Handles confirmation emails sent to users after their order is complete
+    def _send_confirmation_email(self, order):
+        user_email = order.email 
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [user_email]
+        )
+
     
     def handle_event(self, event):
         """
@@ -77,6 +97,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -109,6 +130,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
