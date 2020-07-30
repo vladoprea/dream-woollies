4b951d6fc2e40c14216b04f289d808dd42c8a6cb
Vlad Oprea <opreavladteofil@gmail.com>
2020-07-30 20:48:24 +0100

    Add on sale products to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """ View to return index page """
-    
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+    products_on_sale = products.filter(on_sale=True)
+
+    template = 'home/index.html'
+
+    context = {
+        'products_on_sale': products_on_sale,
+    }
+    return render(request, template, context)
