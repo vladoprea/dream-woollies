4246984ab5b4d3d922d9029c977c042b3fd71c91
Vlad Oprea <opreavladteofil@gmail.com>
2020-08-01 15:28:37 +0100

    Removed secret key

--- dream_woollies/settings.py
+++ dream_woollies/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v)jxtn2lpakh&@dqe4o&0eb2-7p)nttjg+c(ryc$_dkx%#p!-z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
