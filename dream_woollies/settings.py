e4bbeef29829283499c89ebedb6e0bc7ffce7671
Vlad Oprea <opreavladteofil@gmail.com>
2020-08-01 16:52:37 +0100

    Change debug status

--- dream_woollies/settings.py
+++ dream_woollies/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['059bc198daeb.ngrok.io',
                 '127.0.0.1', 
