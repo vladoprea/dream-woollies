ce9772d3fc9b9d22ceaef07be1721eae491e5d27
Vlad Oprea <opreavladteofil@gmail.com>
2020-08-02 20:51:56 +0100

    Add aws S3 for static storage

--- dream_woollies/settings.py
+++ dream_woollies/settings.py
@@ -176,12 +176,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # Whitenoise for static storage
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
