17410e8808d92ffb2be157b59651df3dfe5de581
Vlad Oprea <opreavladteofil@gmail.com>
2020-08-04 10:29:48 +0100

    Change paths for images and logo

--- dream_woollies/settings.py
+++ dream_woollies/settings.py
@@ -183,7 +183,6 @@ STATICFILES_DIRS = (
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
 
--- dream_woollies/urls.py
+++ dream_woollies/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .settings import MEDIA_ROOT
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
