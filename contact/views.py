612bb4eac8b5e5a8c0e19ce5132425dbf3267c67
Vlad Oprea <opreavladteofil@gmail.com>
2020-08-06 14:32:52 +0100

    Add return redirect to view

--- contact/views.py
+++ contact/views.py
@@ -4,15 +4,15 @@ from .forms import ContactForm
 
 def contact_form(request):
     "A view that returns the contact page"
-
+    contact_form = ContactForm()
+    
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid:
             contact_form.save()
             messages.success(request, 'Thank you for contacting us!\
                  We will get back at you in the next 24 hours.')
-    else:
-        contact_form = ContactForm()
+            return redirect('contact')
     
     template = 'contact/contact.html'
 
